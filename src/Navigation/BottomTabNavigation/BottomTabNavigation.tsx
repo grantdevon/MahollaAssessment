271822ee3c42f9edf6c3a240b15846f1c2496ae9
Grant <gsolomons@ssi.engineering>
2023-04-04 22:56:32 +0200

    navigation setup

---
+++ src/Navigation/BottomTabNavigation/BottomTabNavigation.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Discover from '../../Pages/Discover/Discover';
+import Products from '../../Pages/Products/Products';
+
+const Tab = createBottomTabNavigator();
+
+
+const BottomTabNavigation = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Discover" component={Discover} />
+      <Tab.Screen name="Products" component={Products} />
+    </Tab.Navigator>
+  )
+}
+
+export default BottomTabNavigation
+
+const styles = StyleSheet.create({})
