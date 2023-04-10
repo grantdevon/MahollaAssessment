5b6e56167c3c25ccf38ef92172ef542ae353fff0
Grant <gsolomons@ssi.engineering>
2023-04-10 18:58:43 +0200

    search icons

--- android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -11,12 +11,16 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.ArrayList;
 
+// @klarna/react-native-vector-drawable
+import com.klarna.vectordrawable.VectorDrawablePackage;
 // lottie-react-native
 import com.airbnb.android.react.lottie.LottiePackage;
 // react-native-safe-area-context
 import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 // react-native-screens
 import com.swmansion.rnscreens.RNScreensPackage;
+// react-native-vector-icons
+import com.oblador.vectoricons.VectorIconsPackage;
 
 public class PackageList {
   private Application application;
@@ -62,9 +66,11 @@ public class PackageList {
   public ArrayList<ReactPackage> getPackages() {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
       new MainReactPackage(mConfig),
+      new VectorDrawablePackage(),
       new LottiePackage(),
       new SafeAreaContextPackage(),
-      new RNScreensPackage()
+      new RNScreensPackage(),
+      new VectorIconsPackage()
     ));
   }
 }
--- src/Components/Search.component.tsx
+++ src/Components/Search.component.tsx
@@ -1,17 +1,35 @@
-import { StyleSheet, TextInput, View } from 'react-native'
+import { Image, StyleSheet, TextInput, View } from 'react-native'
 import React from 'react'
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const Search = ({searchText, setSearchText}: {searchText:any, setSearchText: any}) => {
+const Search = ({ searchText, setSearchText }: { searchText: any, setSearchText: any }) => {
 
     return (
         <View style={styles.container}>
-            <TextInput 
-            placeholder='search'
-            clearButtonMode="always"
-            value={searchText}
-            onChangeText={setSearchText}
-            />
-            {/* add clear icon */}
+            <View style={styles.subContainer}>
+                <Icon
+                    name='search'
+                    size={20}
+                    color={"black"}
+                />
+                <TextInput
+                    placeholder='Search'
+                    clearButtonMode="always"
+                    value={searchText}
+                    onChangeText={setSearchText}
+                />
+            </View>
+
+            {
+                searchText &&
+                <Icon
+                    name='close'
+                    size={20}
+                    color={"black"}
+                    onPress={() => setSearchText('')}
+                />
+            }
+
         </View>
     )
 }
@@ -20,6 +38,21 @@ export default Search
 
 const styles = StyleSheet.create({
     container: {
-        marginHorizontal: 7,
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        marginVertical: 10,
+        marginHorizontal: 10,
+        paddingHorizontal: 10,
+        borderRadius: 7,
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    subContainer: {
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+    image: {
+        width: 20,
+        height: 20
     }
 })
